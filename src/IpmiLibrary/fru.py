96c609d89ca225a3a2d685902278d14062906b5d
Michael Walle <michael.walle@kontron.com>
2011-06-08 16:47:07 +0200

    refactor fru submodule
    Merge keywords "FRU Data Byte At Offset Should Be" and "FRU Data Bytes At
    Offset Should Be" into one keyword "FRU Data At Offset Should Be". Support
    caching for multiple fru ids.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- src/IpmiLibrary/fru.py
+++ src/IpmiLibrary/fru.py
@@ -5,86 +5,74 @@
 # author: Heiko Thiery <heiko.thiery@kontron.com>
 #
 
+import array
+
 from robot.utils import asserts
+import pyipmi
 
 from utils import int_any_base
 from mapping import *
 
 class Fru:
-    def __init__(self, ipmi):
-        self._fru_data = None
+    def _fru_data(self, fru_id):
+        ac = self._active_connection
+        if (hasattr(ac, '_prefetched_fru_data')
+                and fru_id in ac._prefetched_fru_data):
+            return ac._prefetched_fru_data[fru_id]
+        else:
+            return ac._ipmi.read_fru_data(fru_id)
 
-    def fetch_fru_data(self, fru_id=0):
+    def prefetch_fru_data(self, fru_id=0):
         """Fetches the FRU data of the given `fru_id`.
 
-        Fetching the FRU data is required for all further operation on the FRU
-        data.
+        After prefetching the FRU data, all further operations will use this
+        cached data. Note that every connection has its own cache.
         """
-        ac = self._active_connection
-        ac._fru_data = ac._ipmi.read_fru_data(fru_id)
 
-    def fru_data_byte_at_offset_should_be(self, offset, value, msg=None):
-        """Fails if the FRU data does not contain `value` at the given `offset`
-        """
-        ac = self._active_connection
+        fru_id = int(fru_id)
+        if not hasattr(self._active_connection, '_prefetched_fru_data'):
+            self._active_connection._prefetched_fru_data = {}
+        self._active_connection._prefetched_fru_data[fru_id] = \
+                self._fru_data(fru_id)
+
+    def fru_data_at_offset_should_be(self, offset, expected_data, fru_id=0,
+            msg=None):
+        """Fails unless the FRU data contains the expected data at the given
+        offset."""
+
+        fru_id = int(fru_id)
         offset = int_any_base(offset)
-        value = int_any_base(value)
+        expected_data = [int_any_base(d) for d in expected_data.split(' ')]
 
-        if ac._fru_data is None:
-            self.fetch_fru_data()
+        data = self._fru_data(fru_id)[offset:offset+len(expected_data)]
 
-        asserts.fail_unless_equal(value, ord(ac._fru_data.raw[offset]), msg)
+        # convert to common data structure
+        data = array.array('B', data)
+        expected_data = array.array('B', expected_data)
 
-    def fru_data_bytes_at_offset_should_be(self, offset, length,
-                value_expected, msg=None, order='lsb'):
-        """Fails if the FRU data does not contain `value` at the given `offset`
-        """
-        ac = self._active_connection
-        offset = int_any_base(offset)
-        length = int_any_base(length)
-        value_expected = int_any_base(value_expected)
-
-        if ac._fru_data is None:
-            self.fetch_fru_data()
-
-        value = 0
-        if order == 'lsb':
-            for v in ac._fru_data.raw[offset+length-1:offset-1:-1]:
-                value = (value << 8) + ord(v)
-        elif order == 'msb':
-            for v in ac._fru_data.raw[offset:offset+length]:
-                value = (value << 8) + ord(v)
-        else:
-            raise RuntimeError('Unknown order')
-
-        asserts.fail_unless_equal(value_expected, value, msg)
+        asserts.fail_unless_equal(expected_data, data, msg)
 
     def fru_data_tlv_at_offset_should_be(self, offset, expected_type,
-                expected_length, expected_value, msg=None):
+                expected_length, expected_data, fru_id=0, msg=None):
+        """Fails unless the FRU data contains the TLV triplet at the given
+        offset."""
+
         ac = self._active_connection
         offset = int_any_base(offset)
         expected_type = find_fru_field_type_code(expected_type)
         expected_length = int_any_base(expected_length)
+        fru_id = int(fru_id)
 
-        if ac._fru_data is None:
-            self.fetch_fru_data()
-
-        type = (ord(ac._fru_data.raw[offset]) & 0xc0) >> 6
-        asserts.fail_unless_equal(expected_type, type, 'type missmatch')
-
-        length = ord(ac._fru_data.raw[offset]) & 0x3f
-        asserts.fail_unless_equal(expected_length, length, 'length missmatch')
-
-        value = None
+        # XXX: refactor this, pyipmi already has code for decoding TLVs
         if expected_type == 0:
-            expected_value = int_any_base(expected_value)
-            slice = ac._fru_data.raw[offset+1:offset+length+1]
-            value = 0
-            for v in slice:
-                value = (value << 8) + ord(v)
-        elif expected_type == 3:
-            value = ac._fru_data.raw[offset+1:offset+length+1]
-        else:
-            raise RuntimeError('type %s not supported' % expected_type)
+            # binary
+            expected_data = [int_any_base(d) for d in expected_data.split(' ')]
 
-        asserts.fail_unless_equal(expected_value, value, 'value missmatch')
+        expected_data = array.array('B', str(expected_data))
+
+        tlv = array.array('B',
+                self._fru_data(fru_id)[offset:offset+len(expected_data)+1])
+
+        asserts.fail_unless_equal(expected_type, (tlv[0] >> 6) & 0x3, msg)
+        asserts.fail_unless_equal(expected_length, tlv[0] & 0x3f, msg)
+        asserts.fail_unless_equal(expected_data, tlv[1:], msg)
